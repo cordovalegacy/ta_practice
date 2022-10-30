1e4f36dc39b7d234b060d7339071f86f772ee70c
Brendan Cordova <cordovalegacy19@gmail.com>
2022-10-30 16:18:53 -0700

    vanilla JS practice

---
+++ script.js
@@ -0,0 +1,20 @@
+var neil = 9;
+
+function likesNeil() {
+    neil++;
+    document.querySelector('#neil').innerText = neil;
+}
+
+var nichole = 12;
+
+function likesNichole() {
+    nichole++;
+    document.querySelector('#nichole').innerText = nichole;
+}
+
+var jim = 9;
+
+function likesJim() {
+    jim++;
+    document.querySelector('#jim').innerText = jim;
+}
